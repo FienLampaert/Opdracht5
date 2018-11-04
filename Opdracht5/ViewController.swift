fe1441ce7083efc2f7366867b5bc7ddc60666d14
gebruiker <gebruiker@gebruikers-imac.local>
2018-11-04 15:48:22 +0000

    Initial Commit

---
+++ Opdracht5/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Opdracht5
+//
+//  Created by gebruiker on 04/11/2018.
+//  Copyright © 2018 gebruiker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
